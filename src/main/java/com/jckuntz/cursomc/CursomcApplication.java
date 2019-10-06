1809a597d09dafd1c0ed056fa0de7cccb147fed7
Jonathan Cani <jonathan.kuntz@philips.com>
2019-10-06 10:18:35 -0300

    Create Cidade and Estado repository and update in domains cidade and estado

--- src/main/java/com/jckuntz/cursomc/CursomcApplication.java
+++ src/main/java/com/jckuntz/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.jckuntz.cursomc.domain.Categoria;
+import com.jckuntz.cursomc.domain.Cidade;
+import com.jckuntz.cursomc.domain.Estado;
 import com.jckuntz.cursomc.domain.Produto;
 import com.jckuntz.cursomc.repositories.CategoriaRepository;
+import com.jckuntz.cursomc.repositories.CidadeRepository;
+import com.jckuntz.cursomc.repositories.EstadoRepository;
 import com.jckuntz.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -40,6 +48,19 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(c1, c2));
 		p3.getCategorias().addAll(Arrays.asList(c1));
 		
+		Estado estadoMG = new Estado(null, "Minas Gerais");
+		Estado estadoSP = new Estado(null, "São Paulo");
+		
+		Cidade cidadeUberlandia = new Cidade(null, "Uberlândia", estadoMG);
+		Cidade cidadeSaoPaulo = new Cidade(null, "São Paulo", estadoSP);
+		Cidade cidadeCampinas = new Cidade(null, "Campinas", estadoSP);
+		
+		estadoMG.getCidades().addAll((Arrays.asList(cidadeUberlandia)));
+		estadoSP.getCidades().addAll((Arrays.asList(cidadeSaoPaulo, cidadeCampinas)));
+		
+		estadoRepository.saveAll(Arrays.asList(estadoMG, estadoSP));
+		
+		cidadeRepository.saveAll(Arrays.asList(cidadeUberlandia, cidadeSaoPaulo, cidadeCampinas));		
 		
 		categoriaRepository.saveAll(Arrays.asList(c1,c2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
